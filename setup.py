9530ce7dca2ff3348bb947398a676b64ae5c5788
Rishik00 <96735720+Rishik00@users.noreply.github.com>
2024-09-30 13:01:42 +0530

    configured setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+## For installing dependencies, if any more are to be added they can be included in the pip_libs 
+## or linux_libs list
+
+from typing import List
+import subprocess
+
+linux_libs: List[str] = ['tesseract-ocr', 
+              'tesseract-ocr-hin', 
+              'tesseract-ocr-eng'
+            ]
+
+pip_libs: List[str] = ['pytesseract',
+            'pillow' ,
+            'sentence-transformers', 
+            'paddlepaddle',
+            'paddleocr',
+            'PyPDF2', 
+            'pdf2image', 
+            'langchain', 
+            'langchain_community', 
+            'langchain_groq', 
+            'faiss-cpu'
+        ]
+
+def linux_install(
+        libs_list: List[str]
+    ):
+
+    for lib in libs_list:    
+        print(f'Running for {lib}')
+        
+        linux_install_command = f'sudo apt -qq install {lib}'
+        subprocess.run(linux_install_command, shell=True, check=True)
+
+def pip_install(
+        libs_list: List[str]
+    ):
+
+    for lib in libs_list:
+        print(f'Running for {lib}')
+        
+        pip_command = f'pip install -q {lib}'
+        subprocess.run(pip_command, shell=True, check=True)
+
+if __name__ == "__main__":
+    linux_install(linux_libs)
+    pip_install(pip_libs)
